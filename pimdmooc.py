e9c5d3d9b9950befb18d6c3e3fd254e77ba01cbe
Michele Ceriotti <michele.ceriotti@gmail.com>
2021-09-17 15:53:33 +0200

    Examples of running the simulations

--- pimdmooc.py
+++ pimdmooc.py
@@ -1,9 +1,9 @@
 import sys, os
 
-
-
-
-
 def add_ipi_paths(base="~/i-pi/"):
+    """ Adds to system paths so that one can run from the jupyter 
+    notebooks without any settings after having just cloned the i-PI repo
+    and compiled the FORTRAN driver. """
+    
     sys.path.append(base)
-    os.environ['PATH'] += (base+"/bin/")
+    os.environ['PATH'] += (":"+base+"/bin/")
