7444a2124186d1312b12f5e5cfec38de0768100a
_du64c06c2c8f5747c0 <_du64c06c2c8f5747c0@noto.epfl.ch>
2021-09-21 14:14:19 +0200

    More robust failure when i-PI is missing

--- pimdmooc.py
+++ pimdmooc.py
@@ -1,8 +1,7 @@
 import sys, os
 import ase
-from ipi.utils.io import read_file
 
-def add_ipi_paths(base="~/i-pi/"):
+def add_ipi_paths(base=os.path.expanduser("~")+"/i-pi/"):
     """ Adds to system paths so that one can run from the jupyter 
     notebooks without any settings after having just cloned the i-PI repo
     and compiled the FORTRAN driver. """
@@ -12,6 +11,8 @@ def add_ipi_paths(base="~/i-pi/"):
     
 def read_ipi_xyz(filename):
     """ Reads a file in xyz i-PI format and returns it in ASE format. """
+    
+    from ipi.utils.io import read_file
     file_handle = open(filename, "r")
     frames = []
     while True:
